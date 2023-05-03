706ed12f8592986848d2661d3ad341edde3105c9
Ylixandr <savoshinsky@inbox.ru>
2023-05-03 13:14:33 +0300

    Добавьте файлы проекта.

---
+++ PolessUp_TAsk_20/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = 15;
+bool[] primes = new bool[n + 1];
+int count = 0;
+
+for (int i = 2; i <= n; i++)
+{
+    primes[i] = true;
+}
+
+for (int i = 2; i * i <= n; i++)
+{
+    if (primes[i])
+    {
+        for (int j = i * i; j <= n; j += i)
+        {
+            primes[j] = false;
+        }
+    }
+}
+
+for (int i = 2; i <= n; i++)
+{
+    if (primes[i])
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
+
+//Использовал решето Эратосфена
